9908bfed53df206526b333a6edf019e57485289b
ACER <ACER@NguyenTruongVy>
2026-03-04 19:57:44 +0700

    Nối databse login đến GUI Panel thông tin cá nhân

--- src/main/java/DAO/DatabaseConnection.java
+++ src/main/java/DAO/DatabaseConnection.java
@@ -1,52 +1,37 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package DAO;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-/**
- *
- * @author Admin
- */
 public class DatabaseConnection {
-    private static Connection connection =null;
-    public static Connection getConnection(){
+    
+    private static Connection connection = null;
+    
+    public static Connection getConnection() {
         try {
+            // Khai báo Driver
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            String URL = "jdbc:sqlserver://localhost:1433;databaseName=QuanLyThuVien;trustServerCertificate=true";
-            String UserName="sa";
-<<<<<<< HEAD
-            String Password="nhiheo";
-=======
-            String Password="Phuong2006";
->>>>>>> 6a0ca875679f0335fd3c59213b99632d74efdcc2
             
-            if(connection==null|| connection.isClosed()){
-                connection = DriverManager.getConnection(URL,UserName,Password);   
+            // Đường dẫn kết nối đến SQL Server
+            String URL = "jdbc:sqlserver://localhost:1433;databaseName=QuanLyThuVien;trustServerCertificate=true";
+            String UserName = "sa";
+            String Password = "12345"; // Mật khẩu của bạn
+            
+            // Kiểm tra và mở kết nối
+            if (connection == null || connection.isClosed()) {
+                connection = DriverManager.getConnection(URL, UserName, Password);   
                 System.out.println("Ket noi thanh cong");    
             }
             
         } catch (ClassNotFoundException e) {
             System.out.println("Khong tim thay Driver");
             e.printStackTrace();
-        }
-        catch (SQLException e){
+        } catch (SQLException e) {
             System.out.println("khong the ket noi den Database");
             e.printStackTrace();
         }
-            return connection;
+        
+        return connection;
     }   
-//    // Thêm hàm main này vào để chạy test thử
-//    public static void main(String[] args) {
-//        Connection conn = DatabaseConnection.getConnection();
-//        if (conn != null) {
-//            System.out.println("Chúc mừng! Bạn đã kết nối Database thành công!");
-//        } else {
-//            System.out.println("Kết nối thất bại, hãy kiểm tra lại lỗi ở trên.");
-//        }
-//    }
 }
